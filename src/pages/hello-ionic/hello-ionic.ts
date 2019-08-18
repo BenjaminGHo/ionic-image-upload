9b5fe3aa7ba570a719e1d66f534b490316075a21
Ben Ho <benjamin.g.ho@gmail.com>
2019-08-17 21:35:36 -0400

    Added new functionality to save images to PouchDB

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -20,7 +20,8 @@ export class HelloIonicPage {
 
   
   ngOnInit() {
-    this.getImage = "http://localhost:5985/kittens/mydoc2/testing.jpg";
+
+    //this.getImage = "http://localhost:5985/kittens/mydoc2/testing.jpg";
     /*
     var db = new PouchDB('http://localhost:5985/kittens');
     var url = "";
@@ -38,36 +39,55 @@ export class HelloIonicPage {
       */
   }
 
-  getimage() {
-  
-  }
-
-  getHardCodedImage() {
+  openGallery (): void {
       console.log(1);
-      return "http://localhost:5985/kittens/mydoc1/testing.jpg";
+      let cameraOptions = {
+        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+        destinationType: this.camera.DestinationType.DATA_URL,      
+        quality: 100,
+        targetWidth: 1000,
+        targetHeight: 1000,
+        encodingType: this.camera.EncodingType.JPEG,      
+        correctOrientation: true
+      }
+      let win: any = window;
+      console.log(cameraOptions);
+      this.camera.getPicture(cameraOptions)
+        .then(DATA_URL => 
+          {
+            console.log(DATA_URL);
+            
+            /* allows image to be viewed directly */
+            /*
+            console.log(win.Ionic.WebView.convertFileSrc(file_uri));
+            this.imageSrc = (win.Ionic.WebView.convertFileSrc(file_uri));
+            */
+            this.save(DATA_URL);
+          }, 
+        err => console.log(err));   
   }
 
-   openGallery (): void {
-     console.log(1);
-    let cameraOptions = {
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.FILE_URI,      
-      quality: 100,
-      targetWidth: 1000,
-      targetHeight: 1000,
-      encodingType: this.camera.EncodingType.JPEG,      
-      correctOrientation: true
-    }
-    let win: any = window;
-    console.log(cameraOptions);
-    this.camera.getPicture(cameraOptions)
-      .then(file_uri => 
-        {
-          console.log(file_uri);
-          console.log(win.Ionic.WebView.convertFileSrc(file_uri));
-          this.imageSrc = (win.Ionic.WebView.convertFileSrc(file_uri));
-        }, 
-      err => console.log(err));   
-  }
+   save(DATA_URL) {
+    var _username = '';
+    var _password = '';
+    var remoteDb = new PouchDB(
+      '',
+        {auth: {username: _username, password: _password}});
+        let currentDate = new Date();
+        remoteDb.put({
+          _id: currentDate.toString(),
+          _attachments: {
+          "profile.jpg": {
+            content_type: 'image/jpg',
+            data: DATA_URL
+        }
+      }
+    }).then(function (response){
+      console.log("saved! " + JSON.stringify(response));
+    }).catch(function (err) {
+      console.log(err);
+    });
+  };
+
 
 }
